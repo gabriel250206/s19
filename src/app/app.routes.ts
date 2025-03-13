85f7a03973723ff111d854ba4c99d71679469a36
gabriel250206 <19050@portalcedi.edu.mx>
2025-03-13 08:45:38 -0600

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'tareas',
+    loadComponent: () => import('./tareas/tareas.page').then( m => m.TareasPage)
+  },
 ];
