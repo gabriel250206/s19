ca2e2a591e5484d3e69a80ef6c936862f953597e
Daniela <13044@portalcedi.edu.mx>
2025-03-13 09:09:41 -0600

    directorio

--- src/app/tareas/tareas.page.ts
+++ src/app/tareas/tareas.page.ts
@@ -1,20 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { TaskService, Task } from '../task.service'; 
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { Observable } from 'rxjs';
 @Component({
-  selector: 'app-tareas',
-  templateUrl: './tareas.page.html',
-  styleUrls: ['./tareas.page.scss'],
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonContent, CommonModule, NgFor,FormsModule],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class TareasPage implements OnInit {
+export class HomePage implements OnInit {
+  tasks$: Observable<Task[]> = new Observable<Task[]>();
 
-  constructor() { }
+  constructor(public taskService: TaskService) {}
 
   ngOnInit() {
+    this.tasks$ = this.taskService.getTasks();
+    console.log(this.tasks$);
   }
+  
 
+  addItem(name: string, numtel: string): void {
+    if (name.trim() && numtel.trim()) {
+      const newTask: Task = { 
+        name: name.trim(),
+        numtel: numtel.trim(),
+        completed: false
+      };
+
+      this.taskService.addTask(newTask)
+        .then(() => console.log('número agregado correctamente'))
+        .catch(error => console.error('Error al agregar número:', error));
+    } else {
+      console.error('El nombre y el día no pueden estar vacíos');
+    }
+  }
+  deleteItem(id: string): void {
+    this.taskService.deleteTask(id).then(() => {
+      console.log('número eliminado correctamente');
+    }).catch(error => {
+      console.error('Error al eliminar número:', error);
+    });
+  }
+  editTask(id: string, name: string, numtel: string): void {
+    this.taskService.updateTask(id, { name, numtel }).then(() => {
+      console.log('número actualizado correctamente');
+    }).catch(error => {
+      console.error('Error al actualizar número:', error);
+    });
+  }
+  toggleCompleted(id: string, event: Event): void {
+    const inputElement = event.target as HTMLInputElement; 
+    const completed = inputElement.checked;
+  
+    this.taskService.updateTask(id, { completed }).then(() => {
+      console.log('Estado de número actualizado');
+    }).catch(error => {
+      console.error('Error al actualizar estado:', error);
+    });
+  }
 }
